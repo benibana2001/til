638b32a4ceb297a786c80adbdd400aeb8a0f86fa
benibana2001 <benibana2001@gmail.com>
2019-10-12 00:27:26 +0900

    [20191011]first commit

---
+++ Java/20191011-hello/src/BufferReader.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class BufferReader {
+    public static void main(String[] args) {
+        System.out.println("Input Your Name");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            String line = reader.readLine();
+            System.out.println("Hello " + line);
+
+            System.out.println("Enter Your Age");
+            line = reader.readLine();
+            int age = Integer.parseInt(line);
+
+            System.out.println(age);
+
+        } catch (IOException e) {
+            System.out.println(e);
+        } catch (NumberFormatException e) {
+            System.out.println("Sorry, But Input Age is Not Correct");
+        }
+
+    }
+}
---
+++ Java/20191011-hello/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 10.2;
+
+        System.out.println("iは" + i);
+        System.out.println("dは" + d);
+
+        // ⬇️これが通る...
+        System.out.println("x * dは" + (i * d));
+
+        char c = 'A';
+
+        System.out.println("cは" + c);
+    }
+}
