bfef8f2f3193e9099fa297fb5fa6e6060e2e4b02
benibana2001 <benibana2001@gmail.com>
2019-11-17 12:15:38 +0900

    [20191116/create_react_ts]implement getJSON

--- React/create_react_ts/src/App.tsx
+++ React/create_react_ts/src/App.tsx
@@ -1,6 +1,60 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { resolve } from 'q';
+
+export { View }
+
+class Button extends React.Component<{ value: string, name: string }> {
+  private log = (): void => {
+    console.log(this.props.name)
+  }
+
+  getTopStoryJSON = async (): Promise<any> => {
+    const HKN_TOP_URL: string = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+    let response: any = await fetch(HKN_TOP_URL)
+    return response.json()
+  }
+
+  jsonValid = async(): Promise<any> => {
+    let json: any = await this.getTopStoryJSON()
+    console.log(json)
+    console.log(typeof(json))
+  }
+
+  render() {
+    return (
+      <ul>
+        <li><button onClick={this.log}>{this.props.value}</button></li>
+        <li><button onClick={this.jsonValid}>JSON</button></li>
+      </ul>
+    )
+  }
+}
+
+class Card extends React.Component<{ name: string }>{
+  render() {
+    return (
+      <ul>
+        <li>{this.props.name}</li>
+        <Button value="OK" name={this.props.name} />
+      </ul>
+    )
+  }
+}
+
+class View extends React.Component {
+  render() {
+    return (
+      <div>
+        <Card name="Taro" />
+        <Card name="Alex" />
+        <Card name="Bob" />
+      </div>
+    )
+  }
+}
+
 
 const App: React.FC = () => {
   return (
@@ -22,5 +76,3 @@ const App: React.FC = () => {
     </div>
   );
 }
-
-export default App;
--- React/create_react_ts/src/index.tsx
+++ React/create_react_ts/src/index.tsx
@@ -1,74 +1,13 @@
 import React, { ReactElement, ReactHTMLElement, ReactComponentElement } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { View } from './App';
 import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
-const user: {
-    firstName: string
-    lastName: string
-} = {
-    firstName: 'Ichiro',
-    lastName: 'Suzuki'
-}
 let parent: HTMLElement | null = document.getElementById('root')
-let elem: ReactElement = (
-    <div>
-        <h1>Hello {user.firstName + ' ' + user.lastName}</h1>
-        <h2>Good to see you here.</h2>
-    </div>
-);
-let Welcome = (props: any): ReactElement => {
-    return <h1>Hello {props.name}</h1>
-}
-let Greet = (): ReactElement => {
-    return (
-        <div>
-            <Welcome name="Sara" />
-            <Welcome name="Toma" />
-            <Welcome name="Miku" />
-        </div>
-    )
-}
-class Clock extends React.Component<{}, { date: Date }> {
-    private timerID: NodeJS.Timeout | null
-    constructor(props: any) {
-        super(props);
-        this.state = { date: new Date() }
-        this.timerID = null
-    }
-    tick() {
-        this.setState({
-            date: new Date()
-        })
-    }
-    componentDidMount() {
-        this.timerID = setInterval(
-            () => {
-                this.tick()
-            }, 1000
-        )
-    }
-    componentWillUnmount() {
-        if (this.timerID !== null) clearInterval(this.timerID)
-    }
-    render() {
-        return <div>{this.state.date.toLocaleTimeString()}</div>
-    }
-}
 
-let Clocks = (): ReactElement => {
-    return (
-        <div>
-            <Clock />
-            <Clock />
-            <Clock />
-        </div>
-    )
-}
-
-ReactDOM.render(<Clocks />, parent);
+ReactDOM.render(<View />, parent);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
