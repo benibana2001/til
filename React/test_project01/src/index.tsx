395b7c5789040f623aa69ab45c4e735c1289b8e0
benibana2001 <benibana2001@gmail.com>
2020-01-22 13:27:16 +0900

    [20200122/react/test_project01] Implement Toast Component

--- React/test_project01/src/index.tsx
+++ React/test_project01/src/index.tsx
@@ -7,23 +7,84 @@ const parent: HTMLElement | null = document.getElementById('root')
 //
 interface IState {
     confirmOpen: boolean
+    confirmMessage: string
+    confirmVisible: boolean,
+    countDown: number
 }
 //
 class Apps extends React.Component<{}, IState> {
-    constructor(props: {}){
+    private timer: number = 0
+    //
+    constructor(props: {}) {
         super(props)
         this.state = {
-            confirmOpen: true
+            confirmOpen: false,
+            confirmMessage: "Please hit the confirm button",
+            confirmVisible: true,
+            countDown: 1000
         }
     }
+    //
+    public componentDidMount() {
+        this.timer = window.setInterval(() => { this.handleTimerTick() })
+    }
+    //
+    private handleTimerTick() {
+        this.setState(
+            {
+                confirmMessage:
+                    `Please hit the confirm button ${this.state.countDown} sect to go`,
+                countDown: this.state.countDown - 1
+            },
+            () => {
+                if (this.state.countDown <= 0) {
+                    clearInterval(this.timer)
+                    this.setState({
+                        confirmMessage: "Too late to confirm!",
+                        confirmVisible: false
+                    })
+                }
+            }
+        )
+    }
+    //
     render() {
         return (
-            <Confirm
-                open={this.state.confirmOpen}
-                title="猫山猫美"
-                content="NO CAT NO LIFE! ﾆｬｰ"
-                onOkClick={() => console.log("Ok clicked")}
-                onCancelClick={() => console.log("Cancel clicked")} />
+            <div>
+                <p>{this.state.confirmMessage}</p>
+                {
+                    this.state.confirmVisible && (
+                        <button onClick={() => {
+                            this.setState({ confirmOpen: true })
+                            clearInterval(this.timer)
+                        }}>
+                            Confirm
+                </button>
+                    )
+                }
+
+                <Confirm
+                    open={this.state.confirmOpen}
+                    title="猫山猫美"
+                    content="NO CAT NO LIFE! ﾆｬｰ"
+                    onOkClick={
+                        () => {
+                            this.setState({
+                                confirmOpen: false,
+                                confirmMessage: "Cool, carry on readint!"
+                            })
+                            clearInterval(this.timer)
+                        }
+                    }
+                    onCancelClick={() => {
+                        this.setState({
+                            confirmOpen: false,
+                            confirmMessage: "Take a break, I'm sure you will later"
+                        })
+                        clearInterval(this.timer)
+                    }
+                    } />
+            </div>
         )
     }
 }
