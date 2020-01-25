c5ecaab8d6948e0ad5a7994bcf3c0a92d0393041
benibana2001 <benibana2001@gmail.com>
2020-01-25 17:49:58 +0900

    [20200125/React] Add new directory, shop/

---
+++ React/shop/src/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import './index.scss'
+
+const parent: HTMLElement | null = document.getElementById('root')
+//
+class Apps extends React.Component {
+    render(){
+        return(
+            <div>Hello world!</div>
+        )
+    }
+}
+ReactDOM.render(<Apps />, parent)
