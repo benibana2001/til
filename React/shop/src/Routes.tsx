589e579827445671367251ea3f18103fa4f67757
benibana2001 <benibana2001@gmail.com>
2020-01-26 22:13:10 +0900

    [20200126/React/shop] Edit shop, implemented lazy loading

--- React/shop/src/Routes.tsx
+++ React/shop/src/Routes.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react'
+import { Suspense } from 'react'
 import { BrowserRouter as Router, Redirect, Route, Switch, Link } from 'react-router-dom'
 
 // import Home from './Home'
-import AdminPage from './AdminPage'
+// import AdminPage from './AdminPage'
 import ProductsPage from './ProductsPage'
 import ProductPage from './ProductPage'
 import Header from './Header'
 import NotFoundPage from './NotFoundPage'
 import LoginPage from './LoginPage'
+
+const AdminPage = React.lazy(() => import('./AdminPage'))
 
 const Routes: React.SFC = () => {
     const [loggedIn, setLoggedIn] = React.useState(false)
@@ -21,7 +24,16 @@ const Routes: React.SFC = () => {
                     <Route path="/products/:id" component={ProductPage} />
                     <Route path="/login" component={LoginPage} />
                     <Route path="/admin">
-                        {loggedIn ? <AdminPage /> : <Redirect to="/login" />}
+                        {
+                            loggedIn ? (
+                                <Suspense fallback={
+                                    <div className="page-container">Loading...</div>
+                                }>
+                                    <AdminPage />
+                                </Suspense>
+                            )
+                                : <Redirect to="/login" />
+                        }
                     </Route>
                     <Route component={NotFoundPage} />
                 </Switch>
