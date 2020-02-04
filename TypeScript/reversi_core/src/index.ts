5bcba06a565e0d7287cd757eeaa029e46e0d32fc
benibana2001 <benibana2001@gmail.com>
2020-02-05 01:38:24 +0900

    [20200204/TypeScript] Edit new file for reversi program

--- TypeScript/reversi_core/src/index.ts
+++ TypeScript/reversi_core/src/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello World from your main file!");
+const ROW = 8
+const COLUMN = 8
+const TEST_BOARD = [[0, 0, 0], [1, 1, 1], [2, 2, 2]]
+const funcTimes = (func: Function) => {
+    return (n: number) => {
+        return () => {
+            for (let i = 0; i < n; i++) {
+                func()
+            }
+        }
+    }
+}
+
+const initBoard = (ary: any[]) => {
+    const push = () => ary.push(new Array(COLUMN).fill(0))
+    funcTimes(push)(ROW)()
+}
+
+const getRow = (ary: any[]) => (n: number) => ary[n]
+
+const getColumn = (ary: any[][]) => (n: number) => {
+    let newAry: any[][] = []
+    ary.forEach((elem) => newAry.push(elem[n]))
+    return newAry
+}
+
+let board: any[] = []
+initBoard(board)
+
+console.log(board)
+console.log(getRow(board)(2))
+console.log(getColumn(board)(2))
+
