11217e95393afa972a03ba6f56a31a91859effa9
benibana2001 <benibana2001@gmail.com>
2020-02-10 04:25:57 +0900

    [20200210/python] Edit a file

--- python/20200209/hello.py
+++ python/20200209/hello.py
@@ -22,28 +22,32 @@ DB_NAME = 'vocab.db'
 conn = sqlite3.connect(DB_NAME)
 conn.row_factory = sqlite3.Row
 c = conn.cursor()
-c.execute('DROP VIEW IF EXISTS wordList')
-c.execute('CREATE VIEW IF NOT EXISTS wordList (word, usage, timestamp, book_key)\
-	AS\
-        SELECT DISTINCT W.word, LU.usage, LU.timestamp, LU.book_key\
-	        FROM WORDS AS W LEFT OUTER JOIN LOOKUPS AS LU\
-		ON W.id = LU.word_key\
-			WHERE W.lang = "en"\
-                ORDER BY LU.timestamp DESC')
-c.execute('SELECT word, usage, timestamp, title, authors\
-	FROM wordList AS WL LEFT OUTER JOIN BOOK_INFO AS BI\
-		ON WL.book_key = BI.id')
+c.execute('DROP VIEW IF EXISTS UNIQUE_ENGLISH_WORD')
+c.execute('CREATE VIEW UNIQUE_ENGLISH_WORD\
+	AS SELECT word_key, book_key, usage, timestamp\
+		    FROM LOOKUPS\
+	    WHERE word_key LIKE "en:%"\
+	    GROUP BY word_key;')
+c.execute('SELECT * FROM UNIQUE_ENGLISH_WORD')
 rows = c.fetchall()
 #
+# print(rows)
+# print(rows[0])
+#
 # Iterate Date
-def addDictionary(row):# row
-    d = {}
+# 
+LOOK_UPS = {}
+def addDictionary(row):
     l = list()
     for i, v in enumerate(row.keys()):
         l.append((v, row[i]))
-    d[l[0][1]] = dict(l)
-    return d
+    LOOK_UPS[l[0][1]] = dict(l)
+    return None
 L = list(map(addDictionary, rows))
 f = open("output.json", "w")
-json.dump(L, f, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
+json.dump(LOOK_UPS, f, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
+#
+# c.execute('SELECT id, title, authors FROM BOOK_INFO WHERE lang = "en"')
+# books = c.fetchall()
+#
 conn.close()
