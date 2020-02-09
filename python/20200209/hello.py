86fd690b02c837d776e583438ba602736d9a02a3
benibana2001 <benibana2001@gmail.com>
2020-02-10 03:10:45 +0900

    [20200210/python] Edit a file

--- python/20200209/hello.py
+++ python/20200209/hello.py
@@ -4,18 +4,24 @@ import json
 # Check the document:
 #   https://docs.python.org/2/library/sqlite3.html
 #
-# VARIABLE
-DB_NAME = 'vocab.db'
 # Connect db
 #   Create Row Instance:
 #       https://docs.python.org/ja/3/library/sqlite3.html?highlight=sql#row-objects
 #   Create Cursor Object to call SQL.
+#
+# Create View
+#   We cannnot use placefolder to create view statement.
+#
+# Throw SELECT
+#
+# Parse to JSON
+#
+# Close Connection
+#
+DB_NAME = 'vocab.db'
 conn = sqlite3.connect(DB_NAME)
 conn.row_factory = sqlite3.Row
 c = conn.cursor()
-
-# Create View
-#   We cannnot use placefolder to create view statement.
 c.execute('DROP VIEW IF EXISTS wordList')
 c.execute('CREATE VIEW IF NOT EXISTS wordList (word, usage, timestamp, book_key)\
 	AS\
@@ -24,17 +30,12 @@ c.execute('CREATE VIEW IF NOT EXISTS wordList (word, usage, timestamp, book_key)
 		ON W.id = LU.word_key\
 			WHERE W.lang = "en"\
                 ORDER BY LU.timestamp DESC')
-#
-# Throw SELECT
-#
 c.execute('SELECT word, usage, timestamp, title, authors\
 	FROM wordList AS WL LEFT OUTER JOIN BOOK_INFO AS BI\
 		ON WL.book_key = BI.id')
 rows = c.fetchall()
-print(type(rows))
-# row = rows[0]
 #
-# L = list()
+# Iterate Date
 def addDictionary(row):# row
     d = {}
     l = list()
@@ -42,12 +43,7 @@ def addDictionary(row):# row
         l.append((v, row[i]))
     d[l[0][1]] = dict(l)
     return d
-
 L = list(map(addDictionary, rows))
-# print(L)
-# Parse to JSON
 f = open("output.json", "w")
 json.dump(L, f, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
-# print(json.dumps(d))
-# Close Connection
 conn.close()
