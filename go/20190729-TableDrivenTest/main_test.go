391dc16b30252aa52784e49ada2b75e4d3a3ed53
benibana2001 <benibana2001@gmail.com>
2020-02-10 15:22:27 +0900

    [20190729/go] Add some new files

--- go/20190729-TableDrivenTest/main_test.go
+++ go/20190729-TableDrivenTest/main_test.go
@@ -1,19 +1,36 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestIsEven(t *testing.T) {
 	cases := []struct{
+		name string
 		input int
 		result bool
 	}{
-		{input: 1, result: true},
-		{input: 3, result: false},
+		{name: "even-1", input: 2, result: true},
+		{name: "odd-1", input: 3, result: false},
+		{name: "even-2", input: -2, result: true},
+		{name: "odd-2", input: -3, result: false},
+		{name: "zero", input: 0, result: true},
 	}
 
+	/*
 	for _, c := range cases {
 		if isEven(c.input) != c.result {
-			t.Errorf("Test is failed. expected %v, got %v", c.result, isEven(c.input))
+			t.Errorf("Test is failed. CASE: [%v] ... expected %v, got %v",c.name, c.result, isEven(c.input))
 		}
+	}
+	*/
+
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("%s", tc.name), func(t *testing.T) {
+			if isEven(tc.input) != tc.result {
+				t.Errorf("Test is failed. CASE: [%v] ... expected %v, got %v",tc.name, tc.result, isEven(tc.input))
+			}
+		})
 	}
 }
