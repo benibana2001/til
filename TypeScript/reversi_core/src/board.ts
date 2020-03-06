3793fc87c2f84b73c4f5f898e257651520fb32cf
benibana2001 <benibana2001@gmail.com>
2020-03-07 05:54:32 +0900

    [2020-0307/TypeScript] Edit

--- TypeScript/reversi_core/src/board.ts
+++ TypeScript/reversi_core/src/board.ts
@@ -2,37 +2,65 @@
 const funcTimes = (func: Function) => (n: number) => () => {
     for (let i = 0; i < n; i++) func()
 }
-
-// Migration Array
-const getRow = (ary: number[][]) => (n: number) => ary[n]
-const getColumn = (ary: number[][]) => (n: number) => {
-    let newAry: number[] = []
-    ary.forEach((elem) => newAry.push(elem[n]))
-    return newAry
-}
-const getSquare = (ary: number[][]) => (r: number) => (c: number) => (getRow(ary)(r))[c]
-// 破壊的
-const setSquare = (ary: number[][]) => (n: number) => (r: number) => (c: number) => ary[r][c] = n
-const abs = (n: number) => n < 0 ? - n : n
-const toggle0and1 = (n: 1 | 0) => abs(n - 1)
-
-// Domain Specific
-const ROW = 8
-const COLUMN = 8
-let board: number[][] = []
-const putWhite = setSquare(board)(1)
-const putBlack = setSquare(board)(-1)
-const initBoard = (ary: any[]) => {
-    const push = () => ary.push(new Array(COLUMN).fill(0))
-    funcTimes(push)(ROW)()
-    // Put Initial Token
-    putWhite(3)(3)
-    putWhite(4)(4)
-    putBlack(3)(4)
-    putBlack(4)(3)
+interface IBoard {
+    putWhite: Function
+    putBlack: Function
 }
 
-initBoard(board)
-console.log(board)
+type Square = {
+    row: number,
+    col: number
+}
 
-export { getSquare, getRow, getColumn, abs, toggle0and1, setSquare }
+enum Token {
+    WHITE = 1,
+    BLACK = -1,
+    BLANK = 0
+}
+
+type SquareFunc = (square: Square) => void
+
+class BOARD implements IBoard {
+    private readonly ROW = 8
+    private readonly COLUMN = 8
+    public state: number[][] = []
+
+    public getRow = (ary: number[][]) => (n: number) => ary[n]
+    public getColumn = (ary: number[][]) => (n: number) => {
+        let newAry: number[] = []
+        ary.forEach((elem) => newAry.push(elem[n]))
+        return newAry
+    }
+
+    // public getSquare = (ary: number[][]) => (r: number) => (c: number) => (this.getRow(ary)(r))[c]
+    private setSquare = (ary: number[][]) => (n: number) => (r: number) => (c: number) => ary[r][c] = n
+
+    private put = this.setSquare(this.state)
+    public putWhite = this.put(Token.WHITE)
+    public putBlack = this.put(Token.BLACK)
+
+    private abs = (n: number) => n < 0 ? - n : n
+    // private toggleToken = (n: 1 | 0) => this.abs(n - 1)
+
+    constructor() {
+        const row: number[] = new Array(this.COLUMN).fill(Token.BLANK)
+        const pushRow = () => this.state.push(row.slice(0))
+        const init = funcTimes(pushRow)(this.ROW)
+        init()
+        // Put initial token.
+        this.putWhite(3)(3)
+        this.putWhite(4)(4)
+        this.putBlack(3)(4)
+        this.putBlack(4)(3)
+    }
+
+    public walk = (func: SquareFunc) => {
+        for (let row = 0; row < this.ROW; row++) {
+            for (let col = 0; col < this.COLUMN; col++) {
+                func({ row, col })
+            }
+        }
+    }
+}
+
+export default BOARD
--- TypeScript/reversi_core/src/test/board.test.ts
+++ TypeScript/reversi_core/src/test/board.test.ts
@@ -1,26 +1,21 @@
-import { getRow, abs, toggle0and1, setSquare } from '../board'
+import BOARD from '../board'
 
-const TEST_BOARD = [[0, 0, 0], [1, 1, 1], [2, 2, 2]]
+const board = new BOARD()
 
-test('getRow()', () => {
-    expect(getRow(TEST_BOARD)(0)).toEqual([0, 0, 0])
-    expect(getRow(TEST_BOARD)(1)).toEqual([1, 1, 1])
-    expect(getRow(TEST_BOARD)(2)).toEqual([2, 2, 2])
-})
-
-test('abs()', () => {
-    expect(abs(1)).toEqual(1)
-    expect(abs(-1)).toEqual(1)
-})
-
-test('toggle0and1', () => {
-    expect(toggle0and1(1)).toEqual(0)
-    expect(toggle0and1(0)).toEqual(1)
-})
-
-test('setSquare', () => {
-    const BOARD = TEST_BOARD.map(x => x)
-    const BOARD_BROKEN = [[0, 0, 0], [1, 9, 1], [2, 2, 2]]
-    setSquare(TEST_BOARD)(9)(1)(1)
-    expect(BOARD).toEqual(BOARD_BROKEN)
+describe('TEST TO BOARD', () => {
+    test('initialize', () => {
+        console.log(board.state)
+        expect(board.state).toEqual(
+            [
+                [0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 1, -1, 0, 0, 0],
+                [0, 0, 0, -1, 1, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0],
+            ]
+        )
+    })
 })
