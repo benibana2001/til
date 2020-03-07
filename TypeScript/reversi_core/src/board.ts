9244ded38dc3efaed2556d9741dfe4ee2667bf1f
benibana2001 <benibana2001@gmail.com>
2020-03-08 07:25:40 +0900

    [2020-0308/TypeScript] Implement reverse token function

--- TypeScript/reversi_core/src/board.ts
+++ TypeScript/reversi_core/src/board.ts
@@ -6,18 +6,31 @@ interface IBoard {
     putWhite: Function
     putBlack: Function
 }
-
-type State = number[][]
-type Square = {
-    row: number,
-    col: number
-}
-type SquareFunc = (square: Square) => void
 export enum Token {
     WHITE = 1,
     BLACK = -1,
     BLANK = 0
 }
+type State = {
+    board: BoardState,
+    enablePutSquares: Square[],
+    put: Square | null,
+    reverseToken: Square[],
+    player: Token.WHITE | Token.BLACK
+}
+type BoardState = number[][]
+const initialState: State = {
+    board: [],
+    enablePutSquares: [],
+    put: null,
+    reverseToken: [],
+    player: Token.WHITE
+}
+export type Square = {
+    row: number,
+    col: number
+}
+type SquareFunc = (square: Square) => void
 export type DIRECTION = {
     x: 1 | -1 | 0,
     y: 1 | -1 | 0
@@ -32,43 +45,44 @@ const DIRECTIONS: DIRECTION[] = [
 class BOARD implements IBoard {
     private readonly ROW = 8
     private readonly COLUMN = 8
-    public state: State = []
+    public state: State = initialState
 
     private getRow = (ary: number[][]) => (n: number) => ary[n]
-    private getColumn = (ary: State) => (n: number) => {
+    private getColumn = (ary: BoardState) => (n: number) => {
         let newAry: number[] = []
         ary.forEach((elem) => newAry.push(elem[n]))
         return newAry
     }
 
-    static setSquare = (ary: State) => (n: number) => (r: number) => (c: number) => ary[r][c] = n
+    static setSquare = (ary: BoardState) => (n: number) => (r: number) => (c: number) => ary[r][c] = n
 
-    public putWhite = (row: number, column: number): State => {
-        BOARD.setSquare(this.state)(Token.WHITE)(row)(column)
-        return this.state
+    public putWhite = (row: number, column: number): BoardState => {
+        BOARD.setSquare(this.state.board)(Token.WHITE)(row)(column)
+        return this.state.board
     }
-    public putBlack = (row: number, column: number): State => {
-        BOARD.setSquare(this.state)(Token.BLACK)(row)(column)
-        return this.state
+    public putBlack = (row: number, column: number): BoardState => {
+        BOARD.setSquare(this.state.board)(Token.BLACK)(row)(column)
+        return this.state.board
     }
-    public reverseToken = (row: number, column: number): State => {
-        let square = this.state[row][column]
+    public reverseToken = (row: number, column: number): BoardState => {
+        let square = this.state.board[row][column]
         if (square === Token.BLACK) this.putWhite(row, column)
         if (square === Token.WHITE) this.putBlack(row, column)
-        return this.state
+        return this.state.board
     }
 
     private abs = (n: number) => n < 0 ? - n : n
 
     public initState = (): State => {
-        this.state = []
+        this.state.board = []
         const row: number[] = new Array(this.COLUMN).fill(Token.BLANK)
-        const pushRow = () => this.state.push(row.slice(0))
+        const pushRow = () => this.state.board.push(row.slice(0))
         funcTimes(pushRow)(this.ROW)()
         this.putWhite(3, 3)
         this.putWhite(4, 4)
         this.putBlack(3, 4)
         this.putBlack(4, 3)
+        this.state.enablePutSquares = this.getEnablePutSquares(Token.WHITE)
         return this.state
     }
 
@@ -85,45 +99,75 @@ class BOARD implements IBoard {
     }
 
     // dx, dy: 単位ベクトル
-    public scanLine = (d: DIRECTION) => (x: number, y: number): string => {
-        let patern: string = ''
-        let arr: {
-            x: number
-            y: number
-        }[] = []
+    // public scanLine = (d: DIRECTION) => (x: number, y: number): string => {
+    public scanLine = (d: DIRECTION) => (s: Square): { pattern: string, arr: Square[] } => {
+        let pattern: string = ''
+        let arr: Square[] = []
         for (let m = 1; ; m++) {
-            const currentX = x + d.x * m
-            const currentY = y + d.y * m
-            if (this.state[currentX] === undefined || this.state[currentX][currentY] === undefined) break
-            const square = this.state[currentX][currentY]
-            switch (square) {
+            const currentCol = s.col + d.x * m
+            const currentRow = s.row + d.y * m
+            if (this.state.board[currentRow] === undefined || this.state.board[currentRow][currentCol] === undefined) break
+            const squareVal = this.state.board[currentRow][currentCol]
+            switch (squareVal) {
                 case Token.BLANK:
-                    patern += 'B'
+                    pattern += 'B'
                     break
                 default:
-                    patern += square
+                    pattern += squareVal
             }
-            arr.push({ x: currentX, y: currentY })
+            arr.push({ row: currentRow, col: currentCol })
         }
-        return patern
+        const line = {
+            pattern: pattern,
+            arr: arr
+        }
+        return line
     }
 
-    public enablePutSquares = (t: Token): Square[] => {
+    public getEnablePutSquares = (t: Token): Square[] => {
         let res: Square[] = []
         let player: number = t === Token.WHITE ? Token.WHITE : Token.BLACK
         let enemy: number = t === Token.WHITE ? Token.BLACK : Token.WHITE
         const getEnableSquares: SquareFunc = (s: Square) => {
-            if (this.state[s.row][s.col] !== Token.BLANK) return //  Blankでないマスは配置不可
+            if (this.state.board[s.row][s.col] !== Token.BLANK) return //  Blankでないマスは配置不可
             for (let j = 0; j < DIRECTIONS.length; j++) {
-                const line = this.scanLine(DIRECTIONS[j])(s.row, s.col)
+                const line = this.scanLine(DIRECTIONS[j])(s)
                 const regExStr = new RegExp('^' + enemy + '+' + player)
-                if(line.match(regExStr)) {
+                if (line.pattern.match(regExStr)) {
                     res.push(s)
                 }
             }
         }
         this.walk(getEnableSquares)
         return res
+    }
+
+    public putToken = (s: Square): boolean => {
+        for (let i = 0; i < this.state.enablePutSquares.length; i++) {
+            const square: Square = this.state.enablePutSquares[i]
+            if (square.row === s.row && square.col === s.col) {
+                this.state.put = s
+                // reverse process
+                this.execReverse(s, this.state.player)
+                return true
+            }
+        }
+        return false
+    }
+    private execReverse = (s: Square, player: Token): void => {
+        const enemy = player === Token.WHITE ? Token.BLACK : Token.WHITE
+        for (let i = 0; i < DIRECTIONS.length; i++) {
+            const line = this.scanLine(DIRECTIONS[i])(s)
+            const regExStr = new RegExp('^(' + enemy + '+)' + player)
+            const found = line.pattern.match(regExStr)
+            // To ref about String.match method: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/String/match
+            for (let j = 0; j < found[1].length; j++) {
+                const currentS:Square = line.arr[j]
+                this.state.board[currentS.row][currentS.col] = player
+                this.state.reverseToken.push(currentS)
+            }
+        }
+        this.state.board[s.row][s.col] = player
     }
 }
 
--- TypeScript/reversi_core/src/test/board.test.ts
+++ TypeScript/reversi_core/src/test/board.test.ts
@@ -1,16 +1,16 @@
 import BOARD from '../board'
-import { DIRECTION, Token } from '../board'
+import { DIRECTION, Square, Token } from '../board'
 
-const board = new BOARD()
+const b = new BOARD()
 let t: number = 0
 
 describe('TEST TO BOARD', () => {
     afterEach(() => {
-        board.initState()
+        b.initState()
     })
     test('initialize', () => {
-        console.log(board.state)
-        expect(board.state).toEqual(
+        console.log(b.state.board)
+        expect(b.state.board).toEqual(
             [
                 [0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0],
@@ -24,7 +24,7 @@ describe('TEST TO BOARD', () => {
         )
     })
     test('putToken', () => {
-        expect(board.putWhite(0, 0)).toEqual(
+        expect(b.putWhite(0, 0)).toEqual(
             [
                 [1, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0],
@@ -36,7 +36,7 @@ describe('TEST TO BOARD', () => {
                 [0, 0, 0, 0, 0, 0, 0, 0],
             ]
         )
-        expect(board.putBlack(0, 0)).toEqual(
+        expect(b.putBlack(0, 0)).toEqual(
             [
                 [-1, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0],
@@ -50,7 +50,7 @@ describe('TEST TO BOARD', () => {
         )
     })
     test('reverseToken', () => {
-        expect(board.reverseToken(0, 0)).toEqual(
+        expect(b.reverseToken(0, 0)).toEqual(
             [
                 [0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0],
@@ -62,7 +62,7 @@ describe('TEST TO BOARD', () => {
                 [0, 0, 0, 0, 0, 0, 0, 0],
             ]
         )
-        expect(board.reverseToken(3, 3)).toEqual(
+        expect(b.reverseToken(3, 3)).toEqual(
             [
                 [0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0],
@@ -74,7 +74,7 @@ describe('TEST TO BOARD', () => {
                 [0, 0, 0, 0, 0, 0, 0, 0],
             ]
         )
-        expect(board.reverseToken(3, 3)).toEqual(
+        expect(b.reverseToken(3, 3)).toEqual(
             [
                 [0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0],
@@ -92,23 +92,33 @@ describe('TEST TO BOARD', () => {
         const D2: DIRECTION = { x: 0, y: 1 }
         const D3: DIRECTION = { x: 1, y: 1 }
         const D4: DIRECTION = { x: -1, y: 1 }
-        expect(board.scanLine(D1)(0, 0)).toEqual('BBBBBBB')
-        expect(board.scanLine(D1)(3, 3)).toEqual('-1BBB')
-        expect(board.scanLine(D1)(4, 4)).toEqual('BBB')
-        expect(board.scanLine(D2)(0, 0)).toEqual('BBBBBBB')
-        expect(board.scanLine(D2)(3, 3)).toEqual('-1BBB')
-        expect(board.scanLine(D2)(4, 4)).toEqual('BBB')
-        expect(board.scanLine(D3)(0, 0)).toEqual('BB11BBB')
-        expect(board.scanLine(D4)(7, 0)).toEqual('BB-1-1BBB')
+        const S1: Square = {row: 0, col: 0}
+        const S2: Square = {row: 3, col: 3}
+        const S3: Square = {row: 3, col: 4}
+        const S4: Square = {row: 3, col: 2}
+        const S5: Square = {row: 2, col: 3}
+        const S6: Square = {row: 4, col: 4}
+        const S7: Square = {row: 0, col: 7}
+        expect(b.scanLine(D1)(S1)).toEqual('BBBBBBB')
+        expect(b.scanLine(D1)(S2)).toEqual('-1BBB')
+        expect(b.scanLine(D1)(S3)).toEqual('BBB')
+        expect(b.scanLine(D1)(S4)).toEqual('1-1BBB')
+        expect(b.scanLine(D1)(S5)).toEqual('BBBB')
+        expect(b.scanLine(D1)(S6)).toEqual('BBB')
+        expect(b.scanLine(D2)(S1)).toEqual('BBBBBBB')
+        expect(b.scanLine(D2)(S2)).toEqual('-1BBB')
+        expect(b.scanLine(D2)(S6)).toEqual('BBB')
+        expect(b.scanLine(D3)(S1)).toEqual('BB11BBB')
+        expect(b.scanLine(D4)(S7)).toEqual('BB-1-1BBB')
     })
     test('enablePutSquares', () => {
-        expect(board.enablePutSquares(Token.WHITE)).toEqual([
+        expect(b.getEnablePutSquares(Token.WHITE)).toEqual([
             { "col": 4, "row": 2 },
             { "col": 5, "row": 3 },
             { "col": 2, "row": 4 },
             { "col": 3, "row": 5 }
         ])
-        expect(board.enablePutSquares(Token.BLACK)).toEqual([
+        expect(b.getEnablePutSquares(Token.BLACK)).toEqual([
             { "col": 3, "row": 2 },
             { "col": 2, "row": 3 },
             { "col": 5, "row": 4 },
