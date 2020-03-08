eb0acb3066db6a423ba43f2dec09edf83a9f9488
benibana2001 <benibana2001@gmail.com>
2020-03-08 21:30:49 +0900

    [2020-0308/TypeScript] Test for reverse token function

--- TypeScript/reversi_core/src/board.ts
+++ TypeScript/reversi_core/src/board.ts
@@ -2,14 +2,10 @@
 const funcTimes = (func: Function) => (n: number) => () => {
     for (let i = 0; i < n; i++) func()
 }
-interface IBoard {
-    putWhite: Function
-    putBlack: Function
-}
 export enum Token {
     WHITE = 1,
-    BLACK = -1,
-    BLANK = 0
+    BLACK = 0,
+    BLANK = -1
 }
 type State = {
     board: BoardState,
@@ -41,54 +37,53 @@ const DIRECTIONS: DIRECTION[] = [
     { x: -1, y: 0 }, { x: -1, y: 1 }, { x: -1, y: -1 }
 ]
 
-
-class BOARD implements IBoard {
+class BOARD {
     private readonly ROW = 8
     private readonly COLUMN = 8
+    static getEnemy = (player: Token) => player === Token.WHITE ? Token.BLACK : Token.WHITE
+    static createSquare = (r: number, c: number): Square => ({ row: r, col: c })
     public state: State = initialState
 
-    private getRow = (ary: number[][]) => (n: number) => ary[n]
+    private getRow = (ary: BoardState) => (n: number) => ary[n]
     private getColumn = (ary: BoardState) => (n: number) => {
         let newAry: number[] = []
         ary.forEach((elem) => newAry.push(elem[n]))
         return newAry
     }
-
-    static setSquare = (ary: BoardState) => (n: number) => (r: number) => (c: number) => ary[r][c] = n
-
-    public putWhite = (row: number, column: number): BoardState => {
-        BOARD.setSquare(this.state.board)(Token.WHITE)(row)(column)
+    public getSquare = (s: Square): Token => this.state.board[s.row][s.col]
+    public putSquare = (s: Square) => (player: Token): BoardState => {
+        this.state.board[s.row][s.col] = player
         return this.state.board
     }
-    public putBlack = (row: number, column: number): BoardState => {
-        BOARD.setSquare(this.state.board)(Token.BLACK)(row)(column)
+    public putWhite = (s: Square): BoardState => {
+        this.putSquare(s)(Token.WHITE)
         return this.state.board
     }
-    public reverseToken = (row: number, column: number): BoardState => {
-        let square = this.state.board[row][column]
-        if (square === Token.BLACK) this.putWhite(row, column)
-        if (square === Token.WHITE) this.putBlack(row, column)
+    public putBlack = (s: Square): BoardState => {
+        this.putSquare(s)(Token.BLACK)
         return this.state.board
     }
-
-    private abs = (n: number) => n < 0 ? - n : n
+    public reverseToken = (s: Square): BoardState => {
+        let square = this.getSquare(s)
+        if (square === Token.BLACK) this.putWhite(s)
+        if (square === Token.WHITE) this.putBlack(s)
+        return this.state.board
+    }
 
     public initState = (): State => {
         this.state.board = []
         const row: number[] = new Array(this.COLUMN).fill(Token.BLANK)
         const pushRow = () => this.state.board.push(row.slice(0))
         funcTimes(pushRow)(this.ROW)()
-        this.putWhite(3, 3)
-        this.putWhite(4, 4)
-        this.putBlack(3, 4)
-        this.putBlack(4, 3)
+        this.putWhite({ row: 3, col: 3 })
+        this.putWhite({ row: 4, col: 4 })
+        this.putBlack({ row: 3, col: 4 })
+        this.putBlack({ row: 4, col: 3 })
         this.state.enablePutSquares = this.getEnablePutSquares(Token.WHITE)
         return this.state
     }
 
-    constructor() {
-        this.initState()
-    }
+    constructor() { this.initState() }
 
     public walk = (func: SquareFunc) => {
         for (let row = 0; row < this.ROW; row++) {
@@ -99,35 +94,33 @@ class BOARD implements IBoard {
     }
 
     // dx, dy: 単位ベクトル
-    // public scanLine = (d: DIRECTION) => (x: number, y: number): string => {
     public scanLine = (d: DIRECTION) => (s: Square): { pattern: string, arr: Square[] } => {
         let pattern: string = ''
         let arr: Square[] = []
         for (let m = 1; ; m++) {
-            const currentCol = s.col + d.x * m
-            const currentRow = s.row + d.y * m
-            if (this.state.board[currentRow] === undefined || this.state.board[currentRow][currentCol] === undefined) break
-            const squareVal = this.state.board[currentRow][currentCol]
-            switch (squareVal) {
+            const newSquare: Square = {
+                row: s.row + d.y * m,
+                col: s.col + d.x * m 
+            }
+            if (this.state.board[newSquare.row] === undefined || this.getSquare(newSquare) === undefined) break
+            switch (this.getSquare(newSquare)) {
                 case Token.BLANK:
                     pattern += 'B'
                     break
                 default:
-                    pattern += squareVal
+                    pattern += this.getSquare(newSquare)
             }
-            arr.push({ row: currentRow, col: currentCol })
+            arr.push(newSquare)
         }
-        const line = {
+        return {
             pattern: pattern,
             arr: arr
         }
-        return line
     }
 
-    public getEnablePutSquares = (t: Token): Square[] => {
-        let res: Square[] = []
-        let player: number = t === Token.WHITE ? Token.WHITE : Token.BLACK
-        let enemy: number = t === Token.WHITE ? Token.BLACK : Token.WHITE
+    public getEnablePutSquares = (player: Token): Square[] => {
+        const res: Square[] = []
+        const enemy: number = BOARD.getEnemy(player)
         const getEnableSquares: SquareFunc = (s: Square) => {
             if (this.state.board[s.row][s.col] !== Token.BLANK) return //  Blankでないマスは配置不可
             for (let j = 0; j < DIRECTIONS.length; j++) {
@@ -154,20 +147,27 @@ class BOARD implements IBoard {
         }
         return false
     }
-    private execReverse = (s: Square, player: Token): void => {
-        const enemy = player === Token.WHITE ? Token.BLACK : Token.WHITE
+    /**
+     * 石を置いたのちのアクション
+     */
+    public execReverse = (s: Square, player: Token): Square[] => {
+        let reversed: Square[] = []
+        const enemy = BOARD.getEnemy(player)
         for (let i = 0; i < DIRECTIONS.length; i++) {
-            const line = this.scanLine(DIRECTIONS[i])(s)
+            const line = this.scanLine(DIRECTIONS[i])(s) //  裏返る石のパターン作成
             const regExStr = new RegExp('^(' + enemy + '+)' + player)
             const found = line.pattern.match(regExStr)
             // To ref about String.match method: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/String/match
+            if (found === null) continue
             for (let j = 0; j < found[1].length; j++) {
-                const currentS:Square = line.arr[j]
-                this.state.board[currentS.row][currentS.col] = player
-                this.state.reverseToken.push(currentS)
+                const currentS: Square = line.arr[j]
+                this.putSquare(currentS)(player) //  board 書き換え
+                reversed.push(currentS) //  書き換えた場所を保持
             }
         }
-        this.state.board[s.row][s.col] = player
+        this.putSquare(s)(player)
+        this.state.reverseToken = reversed
+        return this.state.reverseToken 
     }
 }
 
--- TypeScript/reversi_core/src/test/board.test.ts
+++ TypeScript/reversi_core/src/test/board.test.ts
@@ -12,78 +12,81 @@ describe('TEST TO BOARD', () => {
         console.log(b.state.board)
         expect(b.state.board).toEqual(
             [
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 1, -1, 0, 0, 0],
-                [0, 0, 0, -1, 1, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, 1, 0, -1, -1, -1],
+                [-1, -1, -1, 0, 1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
             ]
         )
     })
     test('putToken', () => {
-        expect(b.putWhite(0, 0)).toEqual(
+        const s00 = BOARD.createSquare(0, 0)
+        expect(b.putWhite(s00)).toEqual(
             [
-                [1, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 1, -1, 0, 0, 0],
-                [0, 0, 0, -1, 1, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
+                [1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, 1, 0, -1, -1, -1],
+                [-1, -1, -1, 0, 1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
             ]
         )
-        expect(b.putBlack(0, 0)).toEqual(
+        expect(b.putBlack(s00)).toEqual(
             [
-                [-1, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 1, -1, 0, 0, 0],
-                [0, 0, 0, -1, 1, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
+                [0, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, 1, 0, -1, -1, -1],
+                [-1, -1, -1, 0, 1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
             ]
         )
     })
     test('reverseToken', () => {
-        expect(b.reverseToken(0, 0)).toEqual(
+        const s00 = BOARD.createSquare(0, 0)
+        const s33 = BOARD.createSquare(3, 3)
+        expect(b.reverseToken(s00)).toEqual(
             [
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 1, -1, 0, 0, 0],
-                [0, 0, 0, -1, 1, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, 1, 0, -1, -1, -1],
+                [-1, -1, -1, 0, 1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
             ]
         )
-        expect(b.reverseToken(3, 3)).toEqual(
+        expect(b.reverseToken(s33)).toEqual(
             [
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, -1, -1, 0, 0, 0],
-                [0, 0, 0, -1, 1, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, 0, 0, -1, -1, -1],
+                [-1, -1, -1, 0, 1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
             ]
         )
-        expect(b.reverseToken(3, 3)).toEqual(
+        expect(b.reverseToken(s33)).toEqual(
             [
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 1, -1, 0, 0, 0],
-                [0, 0, 0, -1, 1, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, 1, 0, -1, -1, -1],
+                [-1, -1, -1, 0, 1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1],
             ]
         )
     })
@@ -92,24 +95,63 @@ describe('TEST TO BOARD', () => {
         const D2: DIRECTION = { x: 0, y: 1 }
         const D3: DIRECTION = { x: 1, y: 1 }
         const D4: DIRECTION = { x: -1, y: 1 }
-        const S1: Square = {row: 0, col: 0}
-        const S2: Square = {row: 3, col: 3}
-        const S3: Square = {row: 3, col: 4}
-        const S4: Square = {row: 3, col: 2}
-        const S5: Square = {row: 2, col: 3}
-        const S6: Square = {row: 4, col: 4}
-        const S7: Square = {row: 0, col: 7}
-        expect(b.scanLine(D1)(S1)).toEqual('BBBBBBB')
-        expect(b.scanLine(D1)(S2)).toEqual('-1BBB')
-        expect(b.scanLine(D1)(S3)).toEqual('BBB')
-        expect(b.scanLine(D1)(S4)).toEqual('1-1BBB')
-        expect(b.scanLine(D1)(S5)).toEqual('BBBB')
-        expect(b.scanLine(D1)(S6)).toEqual('BBB')
-        expect(b.scanLine(D2)(S1)).toEqual('BBBBBBB')
-        expect(b.scanLine(D2)(S2)).toEqual('-1BBB')
-        expect(b.scanLine(D2)(S6)).toEqual('BBB')
-        expect(b.scanLine(D3)(S1)).toEqual('BB11BBB')
-        expect(b.scanLine(D4)(S7)).toEqual('BB-1-1BBB')
+        const S1: Square = { row: 0, col: 0 }
+        const S2: Square = { row: 3, col: 3 }
+        const S3: Square = { row: 3, col: 4 }
+        const S4: Square = { row: 3, col: 2 }
+        const S5: Square = { row: 2, col: 3 }
+        const S6: Square = { row: 4, col: 4 }
+        const S7: Square = { row: 0, col: 7 }
+        expect(b.scanLine(D1)(S1)).toEqual({
+            arr: [{
+                row: 0,
+                col: 1
+            }, {
+                row: 0,
+                col: 2
+            }, {
+                row: 0,
+                col: 3
+            }, {
+                row: 0,
+                col: 4
+            }, {
+                row: 0,
+                col: 5
+            }, {
+                row: 0,
+                col: 6
+            }, {
+                row: 0,
+                col: 7
+            }],
+            pattern: 'BBBBBBB'
+        })
+        expect(b.scanLine(D1)(S2)).toEqual({
+            arr: [{
+                row: 3,
+                col: 4
+            }, {
+                row: 3,
+                col: 5
+            }, {
+                row: 3,
+                col: 6
+            }, {
+                row: 3,
+                col: 7
+            }],
+            pattern: '0BBB'
+        })
+        expect(b.scanLine(D1)(S3).pattern).toEqual('BBB')
+        expect(b.scanLine(D1)(S4).pattern).toEqual('10BBB')
+        expect(b.scanLine(D1)(S5).pattern).toEqual('BBBB')
+        expect(b.scanLine(D1)(S6).pattern).toEqual('BBB')
+        expect(b.scanLine(D2)(S1).pattern).toEqual('BBBBBBB')
+        expect(b.scanLine(D2)(S2).pattern).toEqual('0BBB')
+        expect(b.scanLine(D2)(S6).pattern).toEqual('BBB')
+        expect(b.scanLine(D3)(S1).pattern).toEqual('BB11BBB')
+        expect(b.scanLine(D4)(S7).pattern).toEqual('BB00BBB')
     })
     test('enablePutSquares', () => {
         expect(b.getEnablePutSquares(Token.WHITE)).toEqual([
@@ -124,5 +166,20 @@ describe('TEST TO BOARD', () => {
             { "col": 5, "row": 4 },
             { "col": 4, "row": 5 }
         ])
+    })
+    test('execReverse', () => {
+        expect(b.execReverse({ row: 2, col: 4 }, Token.WHITE)).toEqual([
+            {
+                row: 3,
+                col: 4
+            }]
+        )
+        expect(b.execReverse({ row: 4, col: 5 }, Token.BLACK)).toEqual([
+            {
+                row: 4,
+                col: 4
+            },
+        ]
+        )
     })
 })
