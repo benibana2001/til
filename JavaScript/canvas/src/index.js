046978c9cff527f1b870ee88156e4017821b13d0
yusuke <benibana2001@gmail.com>
2020-03-18 02:32:15 +0900

    [2020-03-18/JavaScript/canvas] edit

--- JavaScript/canvas/src/index.js
+++ JavaScript/canvas/src/index.js
@@ -4,5 +4,15 @@ canvas.width = 800
 canvas.height = 600
 root.appendChild(canvas)
 const c = canvas.getContext('2d')
+
+const bgSize = { x: 400, y: 300 }
+const data = [16, 68, 20, 30, 54]
+const offset = 30
+const span = 24
+const barW = (bgSize.x - offset * 2 - span * (data.length - 1)) / data.length 
 c.fillStyle = 'gray'
-c.fillRect(100, 100, 400, 300)
+c.fillRect(0, 0, bgSize.x, bgSize.y)
+c.fillStyle = 'blue'
+for (let i = 0; i < data.length; i++) {
+  c.fillRect(offset + i * (barW + span), offset, barW, data[i] * 3)
+}
