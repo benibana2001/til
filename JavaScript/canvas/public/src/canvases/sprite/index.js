9673dcf319e1b97fe177dbdcf51c95d7e8b4f10f
yusuke <benibana2001@gmail.com>
2020-03-21 19:29:18 +0900

    [2020-03-21/JavaScript/canvas] add a sprite cat image

--- JavaScript/canvas/public/src/canvases/sprite/index.js
+++ JavaScript/canvas/public/src/canvases/sprite/index.js
@@ -1,33 +1,50 @@
 import Canv from '../../CanvWriter.js'
+import catData from './cat.js'
 
 const sprite = async (c) => {
-  const imgPath = '/src/canvases/sprite/sprite.png'
-  const img = Canv.createImg(imgPath)
+  const pathBomb = '/src/canvases/sprite/sprite.png'
+  const pathCat = '/src/canvases/sprite/cat.png'
+  const imgBomb = Canv.createImg(pathBomb)
+  const imgCat = Canv.createImg(pathCat)
   await Canv.waitResolveImgs()
 
-  const size = 12
-  let tick = 0
-  let frame = 0
-  let x = 0
+  let tickB = 0
+  let tickC = 0
+  //
+  const sizeB = 12
+  let frameB = 0
+  let xB = 0
+  //
+  const sizeC = catData.frames["cat 0.aseprite"].spriteSourceSize.w
+  let frameC = 0
+  let xC = 0
 
   Canv.loop(() => {
-    x = frame * size
-    const source = [x, 0, 13, 13]
-    const displayArea = [0, 0, 13, 13]
-    c.drawImage(
-      img,
-      ...source,
-      ...displayArea,
-    )
+    xB = frameB * sizeB
+    const sourceB = [xB, 0, 13, 13]
+    const displayAreaB = [0, 0, 13, 13]
+    c.drawImage(imgBomb, ...sourceB, ...displayAreaB,)
+    console.log(`sizeC: ${sizeC}`)
+    xC = frameC * sizeC
+    const sourceC = [xC, 0, sizeC, sizeC]
+    const displayAreaC = [0, 40, sizeC, sizeC]
+    c.drawImage(imgCat, ...sourceC, ...displayAreaC,)
+
     updateSprite()
-    console.log(frame)
+    console.log(frameB)
   })
   const updateSprite = () => {
-    tick++
-    if (tick % 10 === 0) frame++
-    if (frame >= 10) {
-      tick = 0
-      frame = 0
+    tickB++
+    if (tickB % 10 === 0) frameB++
+    if (frameB >= 10) {
+      tickB = 0
+      frameB = 0
+    }
+    tickC++
+    if (tickC % 20 === 0) frameC++
+    if(frameC >= 3) {
+      tickC = 0
+      frameC = 0
     }
   }
 }
