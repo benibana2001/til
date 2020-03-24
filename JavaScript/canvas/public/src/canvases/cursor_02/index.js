5f3a9d22e43b990ff0ee467f9b335680ec38648b
yusuke <benibana2001@gmail.com>
2020-03-25 04:49:13 +0900

    [2020-03-25/JavaScript/canvas] edit keydown handler

--- JavaScript/canvas/public/src/canvases/cursor_02/index.js
+++ JavaScript/canvas/public/src/canvases/cursor_02/index.js
@@ -1,37 +1,34 @@
 import Canv from '../../CanvWriter.js'
 import personData from './2020-03-24.js'
-const cursor_02 = async() => {
+
+const cursor_02 = async () => {
   const pathPerson = '/src/canvases/cursor_02/2020-03-24.png'
   const imgPerson = Canv.createImg(pathPerson)
   await Canv.waitResolveImgs()
-
-  const move = ((speed) => ({
-    right: {x: speed, y: 0},
-    left: {x: -speed, y: 0},
-    constant: {x: 0, y: 0}
+  // parse json data
+  const frames = Canv.parse(personData)
+  const displayArea = [0, 0]
+  const displaySize = [frames[0][2], frames[0][3]]
+  //
+  const velocity = ((speed) => ({
+    right: { x: speed, y: 0 },
+    left: { x: -speed, y: 0 },
+    constant: { x: 0, y: 0 }
   }))(3)
-  const keydownHandler = e => {
-    const isArrowKey = e => e.key.slice(0, 5) === 'Arrow'
-    if (isArrowKey(e)) e.preventDefault()
-    switch (e.key) {
-      case 'ArrowRight':
-        console.log(e.key)
-        direction = move.right; break
-      case 'ArrowLeft':
-        console.log(e.key)
-        direction = move.left; break
-    }
-  }
-  const keyupHandler = () => {
-    direction = move.constant
-  }
-  let direction = move.constant
+  //
+  let direction = velocity.constant
   const p = Canv.moveParticle({ x: 100, y: 100 })(30)('white')
-  Canv.registerEvent('keydown', keydownHandler)
-  Canv.registerEvent('keyup', keyupHandler)
+  Canv.registerEvent('keydown', Canv.keydownHandler({
+    right: () => direction = velocity.right,
+    left: () => direction = velocity.left
+  }))
+  Canv.registerEvent('keyup', Canv.keyupHandler(
+    () => direction = velocity.constant
+  ))
   Canv.loop(() => {
     Canv.drawBG('black')
     Canv.drawArc(...p(direction))
+    Canv.ctx.drawImage(imgPerson, ...frames[0], ...displayArea, ...displaySize)
   })
 }
 
