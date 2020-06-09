74c95c95ae452403dc00c5807bd720c2ee27f85d
benibana2001 <benibana2001@gmail.com>
2020-06-10 00:44:53 +0900

    [2020-06-10/csp] add a error event to console

--- csp/dist/index.js
+++ csp/dist/index.js
@@ -1,3 +1,4 @@
+window.addEventListener("securitypolicyviolation", console.error.bind(console));
+
 const body = document.body;
-// const str = '<img src=xyz.jpg>'
-body.innerHTML = location.href;
+body.innerHTML = 'abc';
