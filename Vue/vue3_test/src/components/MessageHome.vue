00a4ef43a2e187ea09801594837a667484dd50c9
benibana2001 <benibana2001@gmail.com>
2020-07-29 20:30:48 +0900

    add some files

---
+++ Vue/vue3_test/src/components/MessageHome.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class>{{ message }}</div>
+</template>
+ 
+<script>
+export default {
+  name: "MessageHome",
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
