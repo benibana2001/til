10b0c902a57ea80ea86249da36ddd0006a03e22a
benibana2001 <benibana2001@gmail.com>
2022-05-02 00:22:32 +0900

    2022_05_02

--- ast/test/src/test.js
+++ ast/test/src/test.js
@@ -1,12 +1,11 @@
 const { transform } = require("@babel/core");
 const fs = require("fs");
 const files = ["./sources/declarations.js", "./sources/functions.js"];
-
 files
   .map((file) => fs.readFileSync(file, { encoding: "utf8" }))
   .forEach((src) => {
     const { code } = transform(src, {
-      plugins: [require("./insert_export.js")],
+      plugins: [require("babel-plugin-export-window-declarations")],
     });
     console.log(code);
   });
