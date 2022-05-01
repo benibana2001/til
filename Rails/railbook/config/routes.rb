516d2e6a594f7b86c97a5f1b2102a468200d7aa6
benibana2001 <benibana2001@gmail.com>
2022-05-01 10:42:26 +0900

    delete some project

--- Rails/railbook/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'hello/index'
-  # or get 'hello/index, to: 'hello#index'
-end
