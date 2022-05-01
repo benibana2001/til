22c8732112963f1f9b849f02a73c06c3283b8359
benibana2001 <benibana2001@gmail.com>
2022-05-01 19:56:54 +0900

    2022_05_01

--- ast/test/src/insert_export.js
+++ ast/test/src/insert_export.js
@@ -1,32 +1,39 @@
-const { parseExpression } = require("babylon");
-const { transform } = require("@babel/core");
-
-const WasCreated = Symbol("WasCreated");
-
-// const source = 'function hoge() {}';
-
-const insertCode = 'export {hoge}'
+const exportLiteral = (array) => {
+  if (!Array.isArray(array) || !array.length > 0) return "";
+  return `
+export {
+  ${array.join(",\n  ")}
+}
+`;
+};
 
 const plugin = ({ types: t, template }) => {
+  const pre = function () {
+    this.members = [];
+  };
   const visitor = {
+    VariableDeclarator: (nodePath, state) => {
+      if (!t.isIdentifier(nodePath.node.id)) return;
+      state.members.push(nodePath.node.id.name);
+    },
     FunctionDeclaration: (nodePath, state) => {
       if (!t.isIdentifier(nodePath.node.id)) {
         return;
       }
+      state.members.push(nodePath.node.id.name);
     },
     Program: {
       exit: (nodePath, state) => {
-        const newAst = template(insertCode)();
+        const newAst = template(exportLiteral(state.members))();
         nodePath.pushContainer("body", newAst);
       },
     },
   };
   return {
-      name: 'insert-export',
-      visitor,
-  }
+    name: "insert-export",
+    pre,
+    visitor,
+  };
 };
 
-module.exports = plugin
-
-// console.log(transform(source, { plugins: [plugin] }).code);
+module.exports = plugin;
--- ast/test/src/sources/functions.js
+++ ast/test/src/sources/functions.js
@@ -9,3 +9,13 @@ function b() {
 var c = function () {
   console.log("c");
 };
+
+var d = function (name) {
+  this._name = name;
+};
+
+d.prototype = {
+  _call: function () {
+    console.log(this._name);
+  },
+};
