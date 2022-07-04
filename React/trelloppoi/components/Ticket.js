0b2f85712f8eb74e8b6767c34eced984aff1585b
benibana2001 <benibana2001@gmail.com>
2022-07-05 05:39:15 +0900

    next 2022_07_05
    css while dragging a ticket

--- React/trelloppoi/components/Ticket.js
+++ React/trelloppoi/components/Ticket.js
@@ -5,20 +5,31 @@ import { resetServerContext } from "react-beautiful-dnd";
 export default function Ticket({ ticket, index }) {
   return (
     <Draggable draggableId={ticket.id} index={index}>
-      {(provided) => (
+      {(provided, snapshot) => (
         <div
-          className={styles.ticket}
+          className={
+            snapshot.isDragging
+              ? `${styles.ticket} ${styles.ticket__dragging}`
+              : styles.ticket
+          }
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
         >
-          <div className={styles.main}>
-            <div className={styles.title}>{ticket.content}</div>
-            <div className={styles.avatar}></div>
-          </div>
-          <div className={styles.sub}>
-            <div className={styles.hours}>6</div>
-            <div className={styles.hours}>24</div>
+          <div class={styles.wrapper}>
+            <div className={styles.left}>
+              <span className={styles.content}>{ticket.content}</span>
+              <div className={styles.avatar}></div>
+            </div>
+            <div className={styles.right}>
+              <div>
+                <div className={styles.plus}></div>
+                <div className={`${styles.hours} ${styles.hours__consume}`}>6</div>
+              </div>
+              <div>
+                <div className={`${styles.hours} ${styles.hours__estimate}`}>24</div>
+              </div>
+            </div>
           </div>
         </div>
       )}
--- React/trelloppoi/pages/initail-data.js
+++ React/trelloppoi/pages/initail-data.js
@@ -1,7 +1,7 @@
 export const initialData = {
     tickets: {
       "ticket_1": { id: "ticket_1", content: "Todo 1" },
-      "ticket_2": { id: "ticket_2", content: "Todo 2" },
+      "ticket_2": { id: "ticket_2", content: "Todo_2Todo_2Todo_2Todo_2Todo_2Todo_2Todo_2Todo_2Todo_2Todo_2Todo_2Todo_2Todo_2Todo_2Todo_2Todo_2Todo_2Todo_2Todo_2Todo_2Todo_2" },
       "ticket_3": { id: "ticket_3", content: "Todo 3" },
       "ticket_4": { id: "ticket_4", content: "Todo 4" },
     },
