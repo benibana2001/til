6df943718a87e822722fbf23ecb99c7df7e9ea4b
benibana2001 <benibana2001@gmail.com>
2022-07-09 21:38:58 +0900

    next 2022_07_09
    Button component

--- React/trelloppoi/components/TicketComposer.js
+++ React/trelloppoi/components/TicketComposer.js
@@ -1,6 +1,7 @@
 import Plus from "./Image/Plus";
 import styles from "./scss/TicketComposer.module.scss";
 import { useState, useRef } from "react";
+import Button from "./Button";
 
 export default function TicketComposer({ column, addTicket }) {
   const [content, setContent] = useState("");
@@ -31,21 +32,19 @@ export default function TicketComposer({ column, addTicket }) {
   return (
     <div className={styles.container}>
       {isVisible ? (
-        <div>
-          <textarea
-            placeholder="このチケットのタイトルを入力..."          
-            className={styles.new_ticket}
-            ref={textarea}
-            value={content}
-            onChange={onChangeTextArea}
-            onKeyDown={onKeyDownEnter(onRegister)}
-            onBlur={() => setVisiblity(false)}
-            autoFocus
-          ></textarea>
-          <div className={styles.register_new_ticket} onClick={onRegister}>
-            <div>チケットを追加</div>
-          </div>
-        </div>
+      <div>
+        <textarea
+          placeholder="このチケットのタイトルを入力..."
+          className={styles.new_ticket}
+          ref={textarea}
+          value={content}
+          onChange={onChangeTextArea}
+          onKeyDown={onKeyDownEnter(onRegister)}
+          onBlur={() => setVisiblity(false)}
+          autoFocus
+        ></textarea>
+        <Button text={"チケットを追加"} onClick={onRegister} />
+      </div>
       ) : null}
       <div className={styles.show_new_ticket} onClick={showNewTicket}>
         <div className={styles.plus_wrapper}>
