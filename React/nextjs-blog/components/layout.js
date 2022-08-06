042773d8126ff4b49152812c059a948bbacc8bc1
benibana2001 <benibana2001@gmail.com>
2022-08-07 05:07:33 +0900

    next 2022_08_07

--- React/nextjs-blog/components/layout.js
+++ React/nextjs-blog/components/layout.js
@@ -4,7 +4,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Taro Kobayashi";
+const name = "whasse";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
--- React/nextjs-blog/lib/posts.js
+++ React/nextjs-blog/lib/posts.js
@@ -1,9 +1,11 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import { remark } from "remark";
+import html from "remark-html";
 
 const postsDirectory = path.join(process.cwd(), "pages/posts");
-console.log(postsDirectory)
+console.log(postsDirectory);
 
 export function getSortedPostsDate() {
   const fileNames = fs.readdirSync(postsDirectory);
@@ -32,4 +34,35 @@ export function getSortedPostsDate() {
       return 0;
     }
   });
+}
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ""),
+      },
+    };
+  });
+}
+
+export async function getPostData(id) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  const matterResult = matter(fileContents);
+
+  // convert md int html string
+  const processdContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+  const contentHtml = processdContent.toString();
+
+  return {
+    id,
+    contentHtml,
+    ...matterResult.data,
+  };
 }
