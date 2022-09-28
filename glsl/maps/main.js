178438ce9e453c39fa3deefdb906fd0d3daf446e
benibana2001 <benibana2001@gmail.com>
2022-09-29 05:07:06 +0900

    2022_09_29 glsl

---
+++ glsl/maps/main.js
@@ -0,0 +1,53 @@
+(async () => {
+  const FILE_PATH = "./walking.json";
+
+  // CSVをfetch
+  const fetchJson = async () => {
+    const json = await fetch(FILE_PATH).then((res) => {
+      return res.json();
+    });
+    return json;
+  };
+  const json = await fetchJson();
+
+  // 取得したCSVをパースして２次元配列として保持
+  let points = [];
+  let MAX_SIZE = {
+    x: 0,
+    y: 0,
+  }
+  let MIN_SIZE = {
+    x: 99999,
+    y: 99999,
+  }
+  for(let p in json) {
+    const value = json[p];
+    const ary = value.split(", ").map(xy => {
+      const values = xy.split(" ");
+      return values.map(value => Number(value))
+    });
+    points.push(ary)
+
+    // 最大値をセットしておく
+    for(let point of ary) {
+      if(point[0] > MAX_SIZE.x) {
+        MAX_SIZE.x = point[0]
+      }
+      if(point[1] > MAX_SIZE.y) {
+        MAX_SIZE.y = point[1]
+      }
+      if(point[0] < MIN_SIZE.x) {
+        MIN_SIZE.x = point[0]
+      }
+      if(point[1] < MIN_SIZE.y) {
+        MIN_SIZE.y = point[1]
+      }
+    }
+  }
+  console.log(points)
+  console.log(MAX_SIZE)
+  console.log(MIN_SIZE)
+
+  // 二次元配列を0~1の範囲で正規化
+
+})();
