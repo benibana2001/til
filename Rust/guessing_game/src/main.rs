c812a6ad7dce058d8cc1a67c72fe9999c23495f3
benibana2001 <benibana2001@gmail.com>
2023-01-05 20:20:06 +0900

    The Rust Programming Lnaguage
    2.Programming a Guessing Game

--- Rust/guessing_game/src/main.rs
+++ Rust/guessing_game/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    let mut guess = String::new(); // `mut` means mutable.
+
+    // let apples = 5; // immutable
+
+    io::stdin()
+        .read_line(&mut guess) // `&`はこの引数が参照であることを示す
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
