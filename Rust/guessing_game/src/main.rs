058dd27002d178a6d8fa708fce7ca10f4c983128
benibana2001 <benibana2001@gmail.com>
2023-01-09 14:20:37 +0900

    The Rust Programming Lnaguage
    2.Programming a Guessing Game
    add rand crate

--- Rust/guessing_game/src/main.rs
+++ Rust/guessing_game/src/main.rs
@@ -1,7 +1,13 @@
-use std::io;
+use rand::Rng; // randクレートのRngトレイとを追加
+use std::{cmp::Ordering, io};
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101); //range 1以上101未満
+
+    println!("The secret number is: {}", secret_number);
+
     println!("Please input your guess.");
 
     let mut guess = String::new(); // `mut` means mutable.
@@ -12,5 +18,13 @@ fn main() {
         .read_line(&mut guess) // `&`はこの引数が参照であることを示す
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number"); //shadowing シャドーイング
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"), // Orderingもenumの一つ
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
