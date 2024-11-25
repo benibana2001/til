77c64141bed11d9133916bd8d5af743add758ba8
whasse <benibana2001@gmail.com>
2024-11-25 16:02:48 +0900

    add audio

--- 2024/10/2024_10_31/script.js
+++ 2024/10/2024_10_31/script.js
@@ -83,11 +83,36 @@ function animate() {
 }
 
 function init() {
+  initMusic()
   gameFunctions.createGame()
   animate()
 }
 
 init()
+
+function initMusic() {
+  const audioContext = new AudioContext()
+  const audioElement = document.querySelector("audio")
+  const track = audioContext.createMediaElementSource(audioElement)
+  track.connect(audioContext.destination);
+
+  const playButton = document.querySelector("button")
+  playButton.addEventListener('click', function() {
+    if(audioContext.state === 'suspend') {
+      audioContext.resume()
+    }
+    console.log(this.dataset)
+    if(this.dataset.playing === 'false') {
+      console.log('play')
+      audioElement.play()
+      this.dataset.playing = 'true'
+    }else if(this.dataset.playing === 'true') {
+      console.log('pause')
+      audioElement.pause()
+      this.dataset.playing = 'false'
+    }
+  })
+}
 
 /**
  * DEBUG
